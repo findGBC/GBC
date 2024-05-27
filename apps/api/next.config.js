2223a42df469329619e23a531a8700e7860f4887
Harry89pl <6545499+Harry180@users.noreply.github.com>
2024-05-28 00:02:04 +0200

    fix: remove old way of handling cors

--- apps/api/next.config.js
+++ apps/api/next.config.js
@@ -1,17 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    async headers() {
-        return [
-            {
-                // matching all API routes
-                source: "/api/:path*",
-                headers: [
-                    { key: "Access-Control-Allow-Origin", value: process.env.GBC_DOMAIN },
-                    { key: "Access-Control-Allow-Methods", value: "GET" },
-                ]
-            }
-        ]
-    }
 }
 
 module.exports = nextConfig
